3848397b26e200d2c7b298d7a36d4801f931e962
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-02-27 20:53:14 -0500

    Update tag_list method and delete Tag class

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,14 +9,4 @@ class Article < ApplicationRecord
     end.join(", ")
   end
 
-  class Tag < ActiveRecord::Base
-
-    has_many :taggings
-    has_many :articles, through: :taggings
-
-    def to_s
-      name
-    end
   end
-
-end
