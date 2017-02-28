60494ac26a445fdb5e44c5d766bbe2e39d8486e8
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-02-27 20:41:48 -0500

    Creat tag_list method and Tag class

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,21 @@ class Article < ApplicationRecord
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
+
+  def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(", ")
+  end
+
+  class Tag < ActiveRecord::Base
+
+    has_many :taggings
+    has_many :articles, through: :taggings
+
+    def to_s
+      name
+    end
+  end
+
 end
